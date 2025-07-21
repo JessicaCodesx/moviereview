ebfeec17717feac9f3095b5b52a537b6df049131
Jessica Garcia <139712565+JessicaCodesx@users.noreply.github.com>
2025-07-21 18:29:05 +0000

    fixing search funtionality

--- app/views/movies/index.php
+++ app/views/movies/index.php
@@ -3,7 +3,7 @@
 <div class="search-section">
     <div class="search-box">
         <input type="text" id="searchInput" class="search-input" placeholder="Search for a movie..." />
-        <button onclick="MovieApp.searchMovies()" class="search-btn">Search</button>
+        <button onclick="movieAppInstance.searchMovies()" class="search-btn">Search</button>
     </div>
     <div id="searchResults" class="search-results"></div>
 </div>
