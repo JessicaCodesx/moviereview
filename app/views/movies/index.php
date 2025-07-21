fd9dde472af83a233223841247236aeb169f3251
Jessica Garcia <139712565+JessicaCodesx@users.noreply.github.com>
2025-07-21 17:43:00 +0000

    created movie index

---
+++ app/views/movies/index.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<div class="search-section">
+    <div class="search-box">
+        <input type="text" id="searchInput" class="search-input" placeholder="Search for a movie..." />
+        <button onclick="MovieApp.searchMovies()" class="search-btn">Search</button>
+    </div>
+    <div id="searchResults" class="search-results"></div>
+</div>
+
+<div id="movieDetails" class="movie-details"></div>
